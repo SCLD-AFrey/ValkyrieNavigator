d6e8fda73dbfb485f31f337c58b1fd484f4c18f4
Arthur Frey <afrey@steelcloud.com>
2023-03-14 20:28:50 -0400

    added db

---
+++ ClientApp/Models/Settings.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using ClientApp.Services.Infrastructure;
+
+namespace ClientApp.Models
+{
+    public class Settings
+    {
+        public int LastUserId { get; set; } = 0;
+        private readonly CommonFiles m_commonFilesService;
+
+        public Settings(CommonFiles p_commonFilesService)
+        {
+            m_commonFilesService = p_commonFilesService;
+            
+            if(!File.Exists(m_commonFilesService.SettingsPath))
+                File.Create(m_commonFilesService.SettingsPath);
+            File.WriteAllText(m_commonFilesService.SettingsPath, LastUserId.ToString());
+            
+        }
+
+    }
+}
